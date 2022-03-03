357b8c734d7b3a572897feb8565cc45442f3e966
Polash Mahmud <polashmahmud@gmail.com>
2022-03-03 17:37:07 +0600

    create bills options related files

---
+++ database/migrations/2022_03_03_112337_create_bills_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->string('bill_no');
+            $table->string('bill_month');
+            $table->string('year');
+            $table->date('bill_date');
+            $table->string('bill_amount');
+            $table->date('bill_paid_date')->nullable();
+            $table->string('bill_status')->default('unpaid');
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')
+                ->references('id')
+                ->on('customers')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
